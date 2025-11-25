0019555301a58e8a9f3641b2fc59312411aecdd0
Walaa Hassan <walaa.elbishlawy@gmail.com>
2025-11-25 16:48:49 +0200

    edit get doctor by specialization id

--- Hospital/Hospital.Infrastructure/Repository/DoctorRepository.cs
+++ Hospital/Hospital.Infrastructure/Repository/DoctorRepository.cs
@@ -65,7 +65,7 @@ namespace Hospital.Infrastructure.Repos
                 .Include(d => d.User)
                 .Include(d => d.Specialization)
                 .Include(d => d.Branches)
-                 .Include(d => d.Schedules)
+                .Include(d => d.Schedules)
                 .ToListAsync();
         }
 
@@ -75,6 +75,7 @@ namespace Hospital.Infrastructure.Repos
                 .Include(d => d.User)
                 .Include(d => d.Branches)
                     .ThenInclude(b => b.Specializations)
+                .Include(d => d.Schedules)
                 .Where(d => d.Branches
                     .Any(b => b.Specializations
                         .Any(s => s.SpecializationId == specializationId)))
