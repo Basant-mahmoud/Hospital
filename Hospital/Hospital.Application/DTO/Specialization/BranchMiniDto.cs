e9cfebcb62432cc3406cb4fdb4acc16bb5813868
basant-mahmoud <basant.elshehamy31@gmail.com>
2025-12-07 18:14:15 +0200

    return branch info

--- Hospital/Hospital.Application/DTO/Specialization/BranchMiniDto.cs
+++ Hospital/Hospital.Application/DTO/Specialization/BranchMiniDto.cs
@@ -10,5 +10,13 @@ namespace Hospital.Application.DTO.Specialization
     {
         public int BranchId { get; set; }
         public string BranchName { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Description { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public string? ImageURL { get; set; }
+
     }
 }
